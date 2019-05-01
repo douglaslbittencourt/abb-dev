8a78d28b1df09db4aa6b9750f0faf76bedbb0936
douglaslbittencourt <douglaslbittencourt@gmail.com>
2019-04-30 21:23:07 -0300

    Adicionado o bootstap 4 e ngb, criado novo componente, porem não consegui implementa-lo por enquanto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
+import {CarouselComponent} from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgbCarousel,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
