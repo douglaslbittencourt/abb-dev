ea438bc85d8590f4dd3872880365405643c3fae7
douglaslbittencourt <douglaslbittencourt@gmail.com>
2019-05-02 20:59:03 -0300

    bootstrap funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { NgbCarousel } from '@ng-bootstrap/ng-bootstrap';
-import { CarouselBasicComponent } from './carousel-basic/carousel-basic.component';
 import { AbbMissaoComponent } from './abb-missao/abb-missao.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NgbCarousel,
-    CarouselBasicComponent,
     AbbMissaoComponent
   ],
   imports: [
