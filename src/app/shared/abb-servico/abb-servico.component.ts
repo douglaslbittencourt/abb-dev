546c7a8c9be2d5378bb4845697347a348e515573
douglaslbittencourt <douglaslbittencourt@gmail.com>
2019-05-05 15:55:27 -0300

    Adicionado e implementado o componente Serviço

---
+++ src/app/shared/abb-servico/abb-servico.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-abb-servico',
+  templateUrl: './abb-servico.component.html',
+  styleUrls: ['./abb-servico.component.css']
+})
+export class AbbServicoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/abb-shared.module.ts
+++ src/app/shared/abb-shared.module.ts
@@ -2,14 +2,17 @@ import {NgModule} from '@angular/core';
 import {AbbMissaoComponent} from './abb-missao/abb-missao.component';
 import {TopHeaderComponent} from './top-header/top-header.component';
 import {AbbMapaComponent} from "./abb-mapa/abb-mapa.component";
+import {AbbServicoComponent} from "./abb-servico/abb-servico.component";
+
 
 @NgModule({
   exports: [
     AbbMissaoComponent,
     TopHeaderComponent,
-    AbbMapaComponent
+    AbbMapaComponent,
+    AbbServicoComponent
   ],
-  declarations: [AbbMissaoComponent, TopHeaderComponent, AbbMapaComponent]
+  declarations: [AbbMissaoComponent, TopHeaderComponent, AbbMapaComponent, AbbServicoComponent]
 })
 export class SharedModule {
 
