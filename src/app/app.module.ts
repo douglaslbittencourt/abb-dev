bac0e3c47757a9dae128774019c5da1c734a3ac8
douglaslbittencourt <douglaslbittencourt@gmail.com>
2019-05-09 20:39:25 -0300

    Adicionado componente carousel e implementado, porem falta arrumar a imagem interna

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app.routing.module';
 import {SharedModule} from './shared/abb-shared.module';
 import { AbbHomeModule } from './home/abb-home.module';
 import { AbbServicoModule } from './abb-servico/abb-servico.module';
-import { AbbContatoModule } from "./abb-contato/abb-contato.module";
+import { AbbContatoModule } from './abb-contato/abb-contato.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgbModule.forRoot(),
     RouterModule,
     AbbHomeModule,
     AbbServicoModule,
