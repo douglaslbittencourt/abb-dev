e711b3cf529e1b062ae182f5a95ac49f093957ad
Lucas Bittencourt <lucas_linhares2011@hotmail.com>
2019-05-09 21:22:25 -0300

    Criado arquivos dos parceiros

---
+++ src/app/home/abb-parceiros/abb-parceiros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-abb-parceiros',
+  templateUrl: './abb-parceiros.component.html',
+  styleUrls: ['./abb-parceiros.component.css']
+})
+export class AbbParceirosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
