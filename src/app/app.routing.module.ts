f525c3e7154247d6f6405b79e4cc246dcbc28de7
AndersonHilario <ahj.anderson@gmail.com>
2019-05-13 18:58:39 -0300

    feito roteamento, carousel, header, esboço footer

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AbbHomeComponent } from './home/abb-home.component';
-import {AbbServicoComponent} from './abb-servico/abb-servico.component';
-import {AbbContatoComponent} from './abb-contato/abb-contato.component';
+import { AbbServicoComponent } from './abb-servico/abb-servico.component';
+import { AbbContatoComponent } from './abb-contato/abb-contato.component';
 
 const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },
-    { path: 'home', component: AbbHomeComponent,  data: {title: 'Home'} },
-    { path: 'servico', component: AbbServicoComponent, data: {title: 'Serviço'}},
-    { path: 'contato', component: AbbContatoComponent, data: {title: 'Contato'} },
-    { path: 'sobre', redirectTo: 'home' },
+    { path: 'home', component: AbbHomeComponent, data: { title: 'Home' } },
+    { path: 'servico', component: AbbServicoComponent, data: { title: 'Serviço' } },
+    { path: 'contato', component: AbbContatoComponent, data: { title: 'Contato' } },
+    { path: 'sobre', data: { title: 'Sobre' } },
 ];
 
 @NgModule({
--- src/app/home/abb-home.module.ts
+++ src/app/home/abb-home.module.ts
@@ -5,10 +5,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import {SharedModule} from '../shared/abb-shared.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
     declarations: [AbbHomeComponent, AbbCarousel],
-    imports: [CommonModule, RouterModule, SharedModule, NgbModule]
+    imports: [CommonModule, RouterModule, SharedModule, NgbModule, MDBBootstrapModule]
 })
 export class AbbHomeModule {
 
