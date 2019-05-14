9f761e0ba075e28953692b2dbebd77670dfa368b
douglaslbittencourt <douglaslbittencourt@gmail.com>
2019-05-13 21:38:07 -0300

    Implementado o componente parceiro

--- src/app/home/abb-home.module.ts
+++ src/app/home/abb-home.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { AbbHomeComponent } from './abb-home.component';
 import { AbbCarousel} from './abb-carousel/abb-carousel';
+import { AbbParceirosComponent } from './abb-parceiros/abb-parceiros.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import {SharedModule} from '../shared/abb-shared.module';
@@ -8,8 +9,8 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
-    declarations: [AbbHomeComponent, AbbCarousel],
-    imports: [CommonModule, RouterModule, SharedModule, NgbModule, MDBBootstrapModule]
+    declarations: [AbbHomeComponent, AbbCarousel, AbbParceirosComponent],
+  imports: [CommonModule, RouterModule, SharedModule, NgbModule, MDBBootstrapModule]
 })
 export class AbbHomeModule {
 
--- src/app/home/abb-parceiros/abb-parceiros.component.ts
+++ src/app/home/abb-parceiros/abb-parceiros.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-abb-parceiros',
   templateUrl: './abb-parceiros.component.html',
   styleUrls: ['./abb-parceiros.component.css']
 })
+
 export class AbbParceirosComponent implements OnInit {
 
-  constructor() { }
+  cards = [
+    {
+      img: './assets/images/cliente_logo/cliente_amgem.png'
+    },
+    {
+      img: './assets/images/cliente_logo/cliente_baxter.png'
+    },
+    {
+      img: './assets/images/cliente_logo/cliente_ems.png'
+    },
+    {
+      img: './assets/images/cliente_logo/cliente_ferring.png'
+    },
+    {
+      img: './assets/images/cliente_logo/cliente_libbs.png'
+    },
+    {
+      img: './assets/images/cliente_logo/cliente_shire.png'
+    },
+    {
+      img: './assets/images/cliente_logo/cliente_torrent_farma.png'
+    },
+    {
+      img: './assets/images/cliente_logo/cliente_ucb.png'
+    },
+    {
+      img: './assets/images/cliente_logo/cliente_uniao_quimica.png'
+    },
+    {
+      img: './assets/images/cliente_logo/cliente_valtant.png'
+    },
+    {
+      img: './assets/images/cliente_logo/cliente_libbs.png'
+    },
+    {
+      img: './assets/images/cliente_logo/cliente_ucb.png'
+    },
+  ];
+  slides: any = [[]];
+
+  chunk(arr, chunkSize) {
+    let R = [];
+    for (let i = 0, len = arr.length; i < len; i += chunkSize) {
+      R.push(arr.slice(i, i + chunkSize));
+    }
+    return R;
+  }
 
   ngOnInit() {
+    this.slides = this.chunk(this.cards, 3);
   }
 
 }
