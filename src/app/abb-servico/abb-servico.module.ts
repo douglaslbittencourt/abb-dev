6b4f84c3d325ec308a7fd3b6fc788f79b32fc3e4
douglaslbittencourt <douglaslbittencourt@gmail.com>
2019-05-13 21:52:54 -0300

    Merge

--- src/app/abb-servico/abb-servico.module.ts
+++ src/app/abb-servico/abb-servico.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/abb-shared.module';
 import {AbbServicoComponent} from './abb-servico.component';
 import {AbbSolucoesComponent} from "./abb-solucoes/abb-solucoes.component";
+import {AbbServicosComponent} from "./abb-servicos/abb-servicos.component";
 
 @NgModule({
-  declarations: [AbbServicoComponent, AbbSolucoesComponent],
+  declarations: [AbbServicoComponent, AbbSolucoesComponent, AbbServicosComponent],
     imports: [CommonModule, RouterModule, SharedModule]
 })
 export class AbbServicoModule {
